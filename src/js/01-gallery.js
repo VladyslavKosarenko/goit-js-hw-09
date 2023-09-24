607eff79a4e606c6a712cae18a47b959b9820e31
VladyslavKosarenko <131978971+VladyslavKosarenko@users.noreply.github.com>
2023-09-25 00:34:20 +0300

    start

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,6 @@
-// Add imports above this line
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_LK29Yyc5d6HpqrbOdMOoKlvDw6B1nloufHgqastPSHYeMSNxPI5Cb6DASn28UB4b";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
